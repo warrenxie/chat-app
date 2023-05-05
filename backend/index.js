e5d6483ec1ffd33dd7c7be0e504089e540adc09e
warrenxie <xiewarren@gmail.com>
2023-05-05 00:09:36 -0700

    set up server and fixed font

--- backend/index.js
+++ backend/index.js
@@ -8,16 +8,15 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  try{
+  // Get or create user on Chat Engine!
+  try {
     const r = await axios.put(
       "https://api.chatengine.io/users/",
-      { username: username, secret: username, first_name: username},
-      { headers: {"private-key": "f00d2808-83e1-41cb-92b6-aea4cda8376b"} }
+      { username: username, secret: username, first_name: username },
+      { headers: { "Private-Key": "f00d2808-83e1-41cb-92b6-aea4cda8376b" } }
     );
     return res.status(r.status).json(r.data);
   } catch (e) {
     return res.status(e.response.status).json(e.response.data);
   }
 });
-
-app.listen(3001);
